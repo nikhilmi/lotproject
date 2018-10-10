707c89026a32632725a184d44b4ddce021f68988
YasarArafath07 <myasar2604@gmail.com>
2018-10-10 17:23:44 +0530

    DTO created for LotDto

--- src/main/java/com/teamsankya/lotproject/dto/LotDto.java
+++ src/main/java/com/teamsankya/lotproject/dto/LotDto.java
@@ -1,5 +1,67 @@
 package com.teamsankya.lotproject.dto;
 
-public class LotDto {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class LotDto implements Serializable {
+	@Id
+	@Column
+	private String lotId;
+	private String salesOrderNumber;
+	private String completionClass;
+	private String activeFlag;
+	private long customerId;
+
+	public String getLotId() {
+		return lotId;
+	}
+
+	public void setLotId(String lotId) {
+		this.lotId = lotId;
+	}
+
+	public String getSalesOrderNumber() {
+		return salesOrderNumber;
+	}
+
+	public void setSalesOrderNumber(String salesOrderNumber) {
+		this.salesOrderNumber = salesOrderNumber;
+	}
+
+	public String getCompletionClass() {
+		return completionClass;
+	}
+
+	public void setCompletionClass(String completionClass) {
+		this.completionClass = completionClass;
+	}
+
+	public String getActiveFlag() {
+		return activeFlag;
+	}
+
+	public void setActiveFlag(String activeFlag) {
+		this.activeFlag = activeFlag;
+	}
+
+	public long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(long customerId) {
+		this.customerId = customerId;
+	}
+
+	@Override
+	public String toString() {
+		return "LotDto [lotId=" + lotId + ", salesOrderNumber=" + salesOrderNumber + ", completionClass="
+				+ completionClass + ", activeFlag=" + activeFlag + ", customerId=" + customerId + "]";
+	}
 
 }

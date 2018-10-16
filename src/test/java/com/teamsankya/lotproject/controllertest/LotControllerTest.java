704526cb36d499c59d764855e8df1b2c3c57b14a
ummehabeeba <ummehabeebamca@gmail.com>
2018-10-16 11:49:40 +0530

    added testcases for controller and util classes

--- src/test/java/com/teamsankya/lotproject/controllertest/LotControllerTest.java
+++ src/test/java/com/teamsankya/lotproject/controllertest/LotControllerTest.java
@@ -1,5 +1,27 @@
 package com.teamsankya.lotproject.controllertest;
 
-public class LotControllerTest {
+import org.junit.Test;
+
+import junit.framework.TestCase;
+import net.sourceforge.jwebunit.WebTester;
+
+public class LotControllerTest extends TestCase {
+	private WebTester tester = new WebTester();
+	private static final String BASE_URL = "http://localhost:8080/lotproject/";
+	 
+	@Override
+	protected void setUp() throws Exception {
+		tester.getTestContext().setBaseUrl(BASE_URL);
+	}
+	@Test
+	public void testGetLotId() {
+		tester.beginAt("index.jsp");
+		
+		tester.assertFormPresent();
+		tester.setFormElement("lotId", "2SQT34020.1");
+		//tester.submit();
+		//tester.assertTextPresent("All 3 attributes are for this lot is available..");
+		
+	}
 
 }

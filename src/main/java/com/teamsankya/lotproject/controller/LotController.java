bb4cb6b58af83f687aad544a380adaa493260e12
mehaboobbhai <mehaboobpasha18@gmail.com>
2018-10-16 14:16:13 +0530

    controller is modified.......

--- src/main/java/com/teamsankya/lotproject/controller/LotController.java
+++ src/main/java/com/teamsankya/lotproject/controller/LotController.java
@@ -11,32 +11,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.teamsankya.lotproject.dto.LotBean;
 import com.teamsankya.lotproject.service.LotService;
 
-
-
-
 @Controller
 public class LotController {
 	@Autowired
 	@Qualifier("service")
 	private LotService lotService;
-	
-	final static Logger LOGGER=Logger.getLogger(LotController.class);
+
+	final static Logger LOGGER = Logger.getLogger(LotController.class);
 
 	@RequestMapping(path = "getid", method = RequestMethod.GET)
-	public String getLOTId(ModelMap map,String lotId) {
+	public String getLOTId(ModelMap map, String lotId) {
 		LOGGER.info("inside lot controller");
 		LOGGER.info(lotId);
-		LotBean bean= lotService.getId(lotId);
-		
-		map.addAttribute("bean",lotId);
+		LotBean bean = lotService.getId(lotId);
+
+		map.addAttribute("bean", lotId);
 		map.addAttribute("msg", "All 3 attributes are for this lot is available..");
 		map.addAttribute("msg1", "Entered the lot id is invalid..");
-		if(bean==null) {
+		if (bean == null) {
 			return "Failure";
-		}
-		else
-		return "Success";
-	}	
-
+		} else
+			return "Success";
+	}
 
 }
